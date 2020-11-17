cb294bb56f17061a0de4f7a7a85e8341970809a2
HitLife2k <57799416+HitLife2k@users.noreply.github.com>
2020-11-17 18:13:09 +0700

    new new

--- UserForm.cs
+++ UserForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Data.Entity;
+using System.Data.Entity.Migrations;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -14,134 +15,250 @@ namespace FMSAPP
 {
     public partial class UserForm : Form
     {
-        animeEntities db;
+        animeEntities db; // database context to use
+        OpenFileDialog open; // file chooser dialog
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public UserForm()
         {
             InitializeComponent();
-            DateTimePicker dt = new DateTimePicker();
-            dateTimeDelete.Value = DateTime.Now;
-            dateTimeUpdate.Value = DateTime.Now;
+            currentDate.Value = DateTime.Now; // set current date
+
+            this.CenterToScreen(); // center the form
         }
 
+        /// <summary>
+        /// Load data to form when form loads
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void UserForm_Load(object sender, EventArgs e)
         {
-            db = new animeEntities();
-            db.accounts.Load();
-            accountBindingSource.DataSource = db.accounts.Local;
-            testpicturebox.Image = Image.FromFile(@"../../../FDMSWEB/Content/Images/Avatar/" + Path.GetFileName(txtAvatar.Text));
+            db = new animeEntities(); // instantiate new database context
+            db.accounts.Load(); // load from database
+            accountBindingSource.DataSource = db.accounts.Local.ToBindingList().Where(a => a.deleted_at == null); // load accounts to data source (that are not deleted)
+
+            // loads if there is avatar
+            if (txtAvatar.Text != null && txtAvatar.Text != "")
+            {
+                pbAvatar.Image = Image.FromFile(@"../../../FDMSWEB/Content/Images/users/" + Path.GetFileName(txtAvatar.Text));
+
+            }
+
+            ///* Load roles to combobox */
+            //cbbRoleID.DisplayMember = "name";
+            //cbbRoleID.ValueMember = "RoleID";
             var roleid = db.roles;
             cbbRoleID.DataSource = roleid.ToList();
-            cbbRoleID.DisplayMember = "RoleID";
-            for (int i = 0; i < dataGridView1.Rows.Count; i++)
-            {
-                if (dataGridView1.Rows[i].Cells[8].Value == null && dataGridView1.Rows[i].Cells[2].Value?.ToString() == "2")
-                /* if (dataGridView1.Rows.Cast<DataGridViewRow>()
-      .Any(c => string.IsNullOrWhiteSpace(c.Cells[13].Value?.ToString())))*/
-                {
-                    dataGridView1.Rows[i].Visible = true;
-                }
-                else if (dataGridView1.Rows.Cast<DataGridViewRow>()
-     .Any(c => string.IsNullOrWhiteSpace(c.Cells[8].Value?.ToString())))
-                {
-                    CurrencyManager currencyManager1 = (CurrencyManager)dataGridView1.BindingContext[dataGridView1.DataSource];
-                    currencyManager1.SuspendBinding();
-                    dataGridView1.Rows[i].Visible = false;
-                    currencyManager1.ResumeBinding();
-                }
-            }
+
         }
 
+        /// <summary>
+        /// Open new form for inputting and add new user
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnAdd_Click(object sender, EventArgs e)
         {
+            /* Instantiate new form and show the form */
             AddUserForm auf = new AddUserForm();
             auf.Show();
         }
 
+        /// <summary>
+        /// Delete a user
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnDelete_Click(object sender, EventArgs e)
         {
-            int n = dataGridView1.CurrentRow.Index;
-            dataGridView1.Rows[n].Cells[8].Value = dateTimeDelete.Value.ToString();
-            CurrencyManager currencyManager1 = (CurrencyManager)dataGridView1.BindingContext[dataGridView1.DataSource];
-            currencyManager1.SuspendBinding();
-            dataGridView1.Rows[n].Visible = false;
-            currencyManager1.ResumeBinding();
-            db.SaveChanges();
-            MessageBox.Show("Your data has been successfully delete(or you really think that is)", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            // check if user has selected a row
+            if (userGridView.CurrentRow == null)
+            {
+                MessageBox.Show("Please select a user to delete!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            int n = userGridView.CurrentRow.Index; // get current row index
+
+            try
+            {
+                int deletedId = Convert.ToInt32(userGridView.Rows[n].Cells[0].Value); // get account ID
+                account deletedUser = db.accounts.FirstOrDefault(a => a.AccountID == deletedId); // get account object using ID
+                deletedUser.deleted_at = DateTime.Now; // update deleted_at date
+
+                /* Updates to database */
+                db.accounts.AddOrUpdate(deletedUser);
+                db.SaveChanges();
+
+                /* Empty the input fields and reset combo box to default value */
+                cbbRoleID.SelectedIndex = 0;
+                txtFullName.Text = "";
+                txtEmail.Text = "";
+                cbbGender.Text = "Male";
+                txtAvatar.Text = "";
+                pbAvatar.Image = null;
+
+                // update binding source data
+                accountBindingSource.DataSource = db.accounts.Local.ToBindingList().Where(a => a.deleted_at == null); // load accounts to data source (that are not deleted)
+
+                /* Indicates successful deletion */
+                MessageBox.Show("The user \"" + deletedUser.fullname + "\" has been successfully deleted!", "Delete Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Cannot delete the selected user! Please try again!", "Delete Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
+        /// <summary>
+        /// Update a user
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnUpdate_Click(object sender, EventArgs e)
         {
-            int n = dataGridView1.CurrentRow.Index;
-            dataGridView1.Rows[n].Cells[9].Value = dateTimeUpdate.Value.ToString();
-            db.SaveChanges();
-            MessageBox.Show("Your data has been successfully saved", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
-        }
-
-        private void btnCancel_Click(object sender, EventArgs e)
-        {
-            var changed = db.ChangeTracker.Entries().Where(x => x.State != EntityState.Unchanged).ToList();
-            foreach (var obj in changed)
+            // check if user has selected a row
+            if (userGridView.CurrentRow == null)
             {
-                switch (obj.State)
-                {
-                    case EntityState.Modified:
-                        obj.CurrentValues.SetValues(obj.OriginalValues);
-                        obj.State = EntityState.Unchanged;
-                        break;
-                    case EntityState.Added:
-                        obj.State = EntityState.Detached;
-                        break;
-                    case EntityState.Deleted:
-                        obj.State = EntityState.Unchanged;
-                        break;
-
-                }
+                MessageBox.Show("Please select a user to update!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
             }
-            accountBindingSource.ResetBindings(false);
+
+            int n = userGridView.CurrentRow.Index; // current row index
+            int updateId = Convert.ToInt32(userGridView.Rows[n].Cells[0].Value); // get account ID
+            account updateUser = db.accounts.FirstOrDefault(a => a.AccountID == updateId); // get account object using ID
+
+            // update gender
+            if (cbbGender.Text == "Male")
+            {
+                updateUser.gender = 1;
+            }
+            else if (cbbGender.Text == "Female")
+            {
+                updateUser.gender = 2;
+            }
+            else
+            {
+                updateUser.gender = 3;
+            }
+
+            /* Updates to database */
+            db.accounts.AddOrUpdate(updateUser);
+            db.SaveChanges();
+
+            // update binding source data
+            accountBindingSource.DataSource = db.accounts.Local.ToBindingList().Where(a => a.deleted_at == null); // load accounts to data source (that are not deleted)
+
+            MessageBox.Show("Selected user info has been successfully updated!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            /* Empty the input fields and reset combo box to default value */
+            cbbRoleID.SelectedIndex = 0;
+            txtFullName.Text = "";
+            txtEmail.Text = "";
+            cbbGender.Text = "Male";
+            txtAvatar.Text = "";
+            pbAvatar.Image = null;
+        }
+
+        /// <summary>
+        /// Dispose database context when form closes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void UserForm_FormClosing(object sender, FormClosingEventArgs e)
         {
             db.Dispose();
         }
 
-        private void btnRefesh_Click(object sender, EventArgs e)
+        /// <summary>
+        /// Refresh user grid view
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnRefresh_Click(object sender, EventArgs e)
         {
-            dataGridView1.Refresh();
+            userGridView.Refresh();
+            MessageBox.Show("The grid view has been refreshed!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
-        OpenFileDialog open;
+
+        /// <summary>
+        /// Choose user avatar
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnChoose_Click(object sender, EventArgs e)
         {
-            open = new OpenFileDialog();
+            open = new OpenFileDialog(); // instantiate new instance
+            /* Set some default properties */
             open.Filter = "Images|*.jpg;*.jpeg;*.png";
             open.InitialDirectory = @"C:\";
             open.Title = "Please select an image to update.";
+
+            // if user clicks OK, load the image
             if (open.ShowDialog() == DialogResult.OK)
             {
-                txtAvatar.Text = Path.GetFileName(open.FileName);
-                try
+                int count = 1;
+
+                string fileNameOnly = Path.GetFileNameWithoutExtension(open.FileName);
+                string extension = Path.GetExtension(open.FileName);
+                string path = Path.GetDirectoryName(open.FileName);
+                string newFullPath = open.FileName;
+
+                while (File.Exists(newFullPath))
                 {
-                    File.Copy(open.FileName, @"../../../FDMSWEB/Content/Images/Avatar/" + Path.GetFileName(open.FileName), true);
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Don't change picture from the source file", "This admin is trying to change picture in source",
-    MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
+                    string tempFileName = string.Format("{0}({1})", fileNameOnly, count++);
+                    newFullPath = Path.Combine(path, tempFileName + extension);
+                    txtAvatar.Text = Path.GetFileName(newFullPath);
+                    File.Copy(open.FileName, @"../../../FDMSWEB/Content/Images/users/" + Path.GetFileName(newFullPath));
+                }   
+                /**  try
+                  {
+                      File.Copy(open.FileName, @"../../../FDMSWEB/Content/Images/users/" + Path.GetFileName(open.FileName));
+                  }
+                  catch (Exception ex)
+                  {
+                      MessageBox.Show("Don't change picture from the source file", "This admin is trying to change picture in source", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                  } **/
             }
         }
 
+        /// <summary>
+        /// Update data to gender combo box and picture box on cell click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
         {
+            /* Update combo box gender text */
+            if (userGridView.Rows[e.RowIndex].Cells[6].Value.ToString().Equals("1"))
+            {
+                cbbGender.Text = "Male";
+            }
+            else if (userGridView.Rows[e.RowIndex].Cells[6].Value.ToString().Equals("2"))
+            {
+                cbbGender.Text = "Female";
+            }
+            else
+            {
+                cbbGender.Text = "Other";
+            }
+
+            // check if account has any avatar
             if (txtAvatar.Text == "")
             {
-                testpicturebox.Image = null;
+                pbAvatar.Image = null;
             }
             else
             {
                 try
                 {
-                    testpicturebox.Image = Image.FromFile(@"../../../FDMSWEB/Content/Images/Avatar/" + Path.GetFileName(txtAvatar.Text));
+                    // update account avatar to picture box to display
+                    pbAvatar.Image = Image.FromFile(@"../../../FDMSWEB/Content/Images/users/" + Path.GetFileName(txtAvatar.Text));
                 }
                 catch (Exception exc)
                 {
@@ -151,18 +268,56 @@ MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
+        /// <summary>
+        /// Find users whose username contains find value
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnFind_Click(object sender, EventArgs e)
         {
+            // if find value is null or empty, show all users
             if (string.IsNullOrEmpty(this.txtFind.Text))
             {
-                this.accountBindingSource.DataSource = db.accounts.Local.ToBindingList();
+                accountBindingSource.DataSource = db.accounts.Local.ToBindingList().Where(a => a.deleted_at == null); // load all accounts to data source (that are not deleted)
             }
             else
             {
+                // obtain list of accounts whose usernames contain find value and are not deleted
                 var filteredData = db.accounts.Local.ToBindingList()
-                    .Where(x => x.username.Contains(this.txtFind.Text));
-                this.accountBindingSource.DataSource = filteredData.Count() > 0 ?
+                    .Where(x => x.username.Contains(this.txtFind.Text) && x.deleted_at == null);
+
+                // load filtered data to binding source
+                accountBindingSource.DataSource = filteredData.Count() > 0 ?
                     filteredData : filteredData.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Format all numeric gender values to corresponding strings to display
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void userGridView_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            // check if the column is gender
+            if (userGridView.Columns[e.ColumnIndex].Index == 6)
+            {
+                // check if cell value is null
+                if (e.Value != null)
+                {
+                    if (e.Value.ToString().Equals("1"))
+                    {
+                        e.Value = "Male";
+                    }
+                    else if (e.Value.ToString().Equals("2"))
+                    {
+                        e.Value = "Female";
+                    }
+                    else
+                    {
+                        e.Value = "Other";
+                    }
+                }
             }
         }
     }
